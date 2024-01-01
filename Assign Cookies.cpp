ccbb96a6edbff50437b2bebcc893a91094a60dad
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2024-01-01 17:09:46 +0530

    Create Assign Cookies.cpp

---
+++ Assign Cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int m = g.size();
+        int n = s.size();
+
+        int i=0, j=0;
+
+        while(i < m && j < n){
+            if(g[i] <= s[j]){
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
